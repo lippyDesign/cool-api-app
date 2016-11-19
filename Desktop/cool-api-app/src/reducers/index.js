a03a41c3e227c0f655513d14ca03d691b39d332b
Volodymyr Lipunov <volo_lipu@yahoo.com>
2016-11-19 11:12:57 -0800

    make it better

--- Desktop/cool-api-app/src/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux';
-import YoutubeReducer from './YoutubeReducer';
-
-const rootReducer = combineReducers({
-  youtube: YoutubeReducer,
-});
-
-export default rootReducer;
